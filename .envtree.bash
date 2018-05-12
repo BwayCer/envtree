7766e7ad5d6e6f107c898f6a2c60671ff5da7cbe
BwayCer <bwaycer@gmail.com>
2018-05-12 10:40:00 +0800

    優化 更改 bashBase(bash_base)、bashrc.config(proFile.config) 名稱及目錄位置

--- .envtree.bash
+++ .envtree.bash
@@ -2,8 +2,8 @@
 # 啟用專屬命令行介面
 
 
-if [ -f "$HOME/ys/capp/lib/envtree/.proFile.ysBash" ]; then
-    source "$HOME/ys/capp/lib/envtree/.proFile.ysBash"
+if [ -f "$HOME/.bashrc.ysBash" ]; then
+    source "$HOME/.bashrc.ysBash"
 else
     "$HOME/ys/capp/lib/envtree/ysBash" init
 fi
