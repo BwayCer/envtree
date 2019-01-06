8ac7b8bfd654a10e69c9abf5cba7368c54cf7250
BwayCer <bwaycer@gmail.com>
2019-01-06 23:51:46 +0800

    增補 輸出日誌腳本新增選項用法

--- lib/shbase.loxog.sh
+++ lib/shbase.loxog.sh
@@ -6,16 +6,43 @@
 
 
 source shbase "#fColor"
+source shbase "#parseOption"
 
 
 ##shStyle 腳本環境
 
 
-# loxog <方法 (com|war|err)> [輸出訊息]
-#   例如：
-#     * 一般訊息： loxog com 'txt1' 'txt2'
-#     * 錯誤訊息： loxog err 'txt1' 'txt2' >&2
-#     * 由管道傳遞輸出訊息： echo -e 'txt1\ntxt2' | loxog com
+# loxog
+#    [-f, --fileName <文件名>]
+#    [--stderr]
+#    <方法 (com|war|err)> [輸出訊息]
+#
+# 使用說明：
+#   * 一般訊息： loxog com 'txt1' 'txt2'
+#   * 錯誤訊息： loxog --stderr err 'txt1' 'txt2'
+#   * 由管道傳遞輸出訊息： echo -e 'txt1\ntxt2' | loxog com
+
+[ -n "$_shBase_loadfile" ] \
+    && loxog_fileName=`basename "$_shBase_loadfile"` \
+    || loxog_fileName="loxog"
+
+loxog_opt_fileName=""
+loxog_opt_stderr=0
+loxog_opt() {
+    case "$1" in
+        -f | --fileName )
+            [ -z "$2" ] && return 4
+
+            loxog_opt_fileName="$2"
+            return 2
+            ;;
+        --stderr )
+            loxog_opt_stderr=1
+            return 1
+            ;;
+        * ) return 3 ;;
+    esac
+}
 loxog() {
     # 若非由終端輸入則由管道讀取值
     local _stdin=`[ ! -t 0 ] && { \
@@ -23,10 +50,16 @@ loxog() {
         while read pipeData; do echo "$pipeData"; done <&0;
         unset IFS
     }`
-    local method="$1"; shift
 
-    local idx val len
+    parseOption "$loxog_fileName" "loxog" "$@"
+    local args=("${rtnParseOption[@]}")
+
+    local method="${args[0]}"; args=("${args[@]:1}")
+
+    local line
     local color formatArgus
+    local prefixSource=""
+    local outTxt=""
 
     case $method in
         com ) color=$_fN    ;; # common
@@ -34,17 +67,30 @@ loxog() {
         err ) color=$_fRedB ;; # error
     esac
 
-    formatArgus="$color%s$_fN\n"
+    if [ -n "$loxog_opt_fileName" ]; then
+        prefixSource="[$loxog_opt_fileName]: "
+    fi
 
-    [ -n "$*" ] && printf "$formatArgus" "$@"
+    formatArgus="$color$prefixSource%s$_fN$_br"
+
+    [ -n "${args[*]}" ] \
+        && outTxt+="`printf "$formatArgus" "${args[@]}"`$_br"
 
     if [ -n "$_stdin" ]; then
-        len=`echo "$_stdin" | wc -l`
-        for idx in `seq 1 $len`
-        do
-            val=`echo "$_stdin" | sed -n "${idx}p"`
-            printf "$formatArgus" "$val"
-        done
+        outTxt+="`{ \
+            IFS='';
+            while read line
+            do
+                printf "$formatArgus" "$line"
+            done <<< "$_stdin"
+            unset IFS
+        }`$_br"
+    fi
+
+    if [ $loxog_opt_stderr -ne 1 ]; then
+        printf "$outTxt"
+    else
+        printf "$outTxt" >&2
     fi
 }
 
