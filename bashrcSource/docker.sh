7f4286fc762d79e135a3d39595aa47384b809c1d
BwayCer <bwaycer@gmail.com>
2019-04-14 19:46:40 +0800

    增補 mizarch 預設環境

---
+++ bashrcSource/docker.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+# bashrcSource/docker.sh
+
+
+##shStyle ###
+
+
+# __ysBashPath
+# __envCode
+
+
+##shStyle ###
+
+
+alias docker.once.mizarch="docker.once --image local/mizarch:latest --bway --network host bash"
+
