cc29d4c8d1d208ad5b5c00f3bbb97f3446957389
BwayCer <bwaycer@gmail.com>
2019-05-06 06:37:48 +0800

    增補 建立命令自動補齊的執行文件

--- ysBashBuild/install-k8sTool.sh
+++ ysBashBuild/install-k8sTool.sh
@@ -20,6 +20,7 @@
 
 
 k8sToolPath=`realpath "$_dirsh/.."`
+bashrcSourcePath="$k8sToolPath/bashrcSource"
 binPath="$k8sToolPath/bin"
 
 googleStorageUrl="https://storage.googleapis.com"
@@ -32,20 +33,30 @@ fnBuild_kubectl() {
     local latestVersion=`curl -s $googleStorageUrl/kubernetes-release/release/stable.txt`
     local remoteFileUrl="$googleStorageUrl/kubernetes-release/release/$latestVersion/bin/linux/amd64/kubectl"
     local storePath="$binPath/kubectl"
+    local completionFilePath="$bashrcSourcePath/completion-kubectl"
 
     if [ ! -e "$storePath" ]; then
         curl -L "$remoteFileUrl" -o "$storePath"
         chmod 755 "$storePath"
+    fi
+    if [ ! -e "$completionFilePath" ]; then
+        "$storePath" "completion" "bash" > "$completionFilePath"
+        chmod 755 "$completionFilePath"
     fi
 }
 
 fnBuild_minikube() {
     local remoteFileUrl="$googleStorageUrl/minikube/releases/latest/minikube-linux-amd64"
     local storePath="$binPath/minikube"
+    local completionFilePath="$bashrcSourcePath/completion-minikube"
 
     if [ ! -e "$storePath" ]; then
         curl -L "$remoteFileUrl" -o "$storePath"
         chmod 755 "$storePath"
+    fi
+    if [ ! -e "$completionFilePath" ]; then
+        "$storePath" "completion" "bash" > "$completionFilePath"
+        chmod 755 "$completionFilePath"
     fi
 }
 
@@ -53,7 +64,8 @@ fnBuild_minikube() {
 ##shStyle ###
 
 
-[ -d "$binPath"       ] || mkdir -p "$binPath"
+[ -d "$bashrcSourcePath" ] || mkdir -p "$bashrcSourcePath"
+[ -d "$binPath"          ] || mkdir -p "$binPath"
 
 fnBuild_kubectl
 fnBuild_minikube
