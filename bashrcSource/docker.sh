1ff8e8254f27830d9ecbed7700dbdcba41af921b
BwayCer <bwaycer@gmail.com>
2019-07-11 00:15:33 +0800

    優化 單次執行腳本 區分易混淆的 bway bwaycer 參數更改

--- bashrcSource/docker.sh
+++ bashrcSource/docker.sh
@@ -12,6 +12,6 @@
 ##shStyle ###
 
 
-alias docker.once.mizarchRun="docker.once --image local/mizarch:latest --bway --network host"
+alias docker.once.mizarchRun="docker.once --image local/mizarch:latest --bwaycer --myhome --network host"
 alias docker.once.mizarch="docker.once.mizarchRun bash"
 
