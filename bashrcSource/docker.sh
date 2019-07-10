d6a97b1ec4a29a08fe4525521bb7fbbd076713e2
BwayCer <bwaycer@gmail.com>
2019-07-10 23:49:32 +0800

    增補 取得容器地址的命令別名

--- bashrcSource/docker.sh
+++ bashrcSource/docker.sh
@@ -12,6 +12,9 @@
 ##shStyle ###
 
 
+# docker.ipa <容器 (Name|ID) ...>
+alias docker.ipa="docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}'"
+
 alias docker.once.mizarchRun="docker.once --image local/mizarch:latest --bwaycer --myhome --network host"
 alias docker.once.mizarch="docker.once.mizarchRun bash"
 
