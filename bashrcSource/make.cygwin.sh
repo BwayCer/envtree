9177018aabeea7d925894dec7c338314064df5cc
BwayCer <bwaycer@gmail.com>
2019-08-19 18:04:47 +0800

    201903030247 tasklist, taskkill

--- bashrcSource/make.cygwin.sh
+++ bashrcSource/make.cygwin.sh
@@ -16,3 +16,8 @@ tmpPath="`cat "$__ysBashPath/capp/lib/make.cygwin/lib/_cappPath.config"`/.bin"
 [[ ":$PATH:" =~ ":$tmpPath:" ]] || PATH="$PATH:$tmpPath"
 unset tmpPath
 
+
+# https://superuser.com/questions/18830
+# https://blog.twtnn.com/2013/11/windowstasklisttaskkillprocess.html
+alias taskkill.f.PID="taskkill.exe /f /PID"
+
