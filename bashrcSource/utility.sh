bb8bc6402dc62dd88d59564bf47b7eeea700d00f
BwayCer <bwaycer@gmail.com>
2019-09-23 13:45:21 +0800

    temp 2019-09-23T05:45:21.742612484Z

--- bashrcSource/utility.sh
+++ bashrcSource/utility.sh
@@ -14,3 +14,14 @@
 
 alias date.RFC3339NanoZ08='date -u +"%Y-%m-%dT%H:%M:%S.%9NZ"'
 
+
+# VirtualBox
+
+alias vmtool.box.mount="sudo mount -t vboxsf -o uid=1000,gid=1000" # <資料夾名稱> <dirpath>
+
+
+# protobuf
+
+alias protoc.go="protoc --go_out"
+alias protoc.js="protoc --js_out"
+
